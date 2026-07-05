97a176e6fe61c9c75560b1db374a044f4889669c
LSDArroyoGold <lsdarroyogold@gmail.com>
2026-07-05 06:28:35 -0300

    Update inicio_atardecer.sh

--- scripts/inicio_atardecer.sh
+++ scripts/inicio_atardecer.sh
@@ -5,12 +5,15 @@ REAL_USER="${SUDO_USER:-$(whoami)}"
 USER_HOME="$(getent passwd "$REAL_USER" | cut -d: -f6)"
 export RCLONE_CONFIG="$USER_HOME/.config/rclone/rclone.conf"
 export HOME="$USER_HOME"
+
 CONFIG_HORARIOS="$BASE_PATH/config/config_horarios.txt"
 CONFIG_GENERAL="$BASE_PATH/config/config_general.txt"
+
 DRIVE_PATH=$(awk -F'=' '/^DRIVE_PATH=/{print $2}' "$CONFIG_GENERAL" | tr -d '\r')
 HORARIO=$(awk -F'=' '/INICIO_ATARDECER/{print $2}' "$CONFIG_HORARIOS" |  tr -d '\r')
 HORA_ACTUAL=$(date +%H:%M)
 HORARIO_DELAY=$(echo "$HORARIO" | awk -F: '{m=$2+2; h=$1; if(m>=60){m=m-60} printf "%02d:%02d\n", h, m}')
+
 if [ "$HORA_ACTUAL" = "$HORARIO_DELAY" ]; then
 	UMBRAL=$(python3 -c "
 import sys
@@ -34,6 +37,7 @@ capacidad_wh = (CAPACIDAD_MAH/1000)*VOLTAJE
 umbral = (CONSUMO_WH/capacidad_wh)*100*MARGEN
 print(int(umbral))
 ")
+
 	NIVEL=$(python3 -c "
 import sys
 sys.path.append('/home/lsd/BirdNET-Pi/PiJuice/Software/Source')
@@ -41,6 +45,7 @@ from pijuice import PiJuice
 pj = PiJuice(1, 0x14)
 print(pj.status.GetChargeLevel()['data'])
 ")
+
 	if [ "$NIVEL" -lt "$UMBRAL" ]; then
 		python3 "$BASE_PATH/python/log_sistema.py" CANCELADA atardecer
 		sudo nmcli radio wifi on
@@ -54,8 +59,12 @@ print(pj.status.GetChargeLevel()['data'])
 		fi
 		sudo nmcli radio wifi off
 		sudo chown "$REAL_USER:$REAL_USER" "$USER_HOME/.config/rclone/rclone.conf"
+
 		HORA_WAKE=$(awk -F'=' '/INICIO_AMANECER/{print $2}' "$CONFIG_HORARIOS" | tr -d '\r')
 		python3 "$BASE_PATH/python/set_wake_pijuice.py" $HORA_WAKE
+		python3 "$BASE_PATH/python/log_sistema.py" MSG "Próxima ventana: amanecer a las $HORA_WAKE"
+		python3 "$BASE_PATH/python/log_sistema.py" MSG "Alarma programada para $HORA_WAKE. Apagando."
+
 		python3 -c "
 import sys
 sys.path.append('/home/lsd/BirdNET-Pi/PiJuice/Software/Source')
@@ -66,6 +75,9 @@ pj.power.SetPowerOff(30)
 		sudo poweroff
 	else
 		python3 "$BASE_PATH/python/log_sistema.py" INICIO atardecer
+		HORA_CIERRE=$(awk -F'=' '/FIN_ATARDECER/{print $2}' "$CONFIG_HORARIOS" | tr -d ' \r')
+		python3 "$BASE_PATH/python/log_sistema.py" MSG "Cierre de ventana a las $HORA_CIERRE"
+
 		sudo nmcli radio wifi on
 		INTENTOS=0
 		until ping -c 1 google.com &>/dev/null || [ $INTENTOS -ge 6 ]; do
