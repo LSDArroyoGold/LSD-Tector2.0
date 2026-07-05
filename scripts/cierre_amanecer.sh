81b4743a41fbd01c118857f1e9dacd3e642e5689
LSDArroyoGold <lsdarroyogold@gmail.com>
2026-07-05 06:16:47 -0300

    Update cierre_amanecer.sh

--- scripts/cierre_amanecer.sh
+++ scripts/cierre_amanecer.sh
@@ -28,16 +28,25 @@ if [ "$HORA_ACTUAL" = "$HORARIO" ]; then
 
 	if ! ping -c 1 google.com &>/dev/null; then
 		sudo nmcli radio wifi off
+
 		find "$USER_HOME/BirdSongs/Extracted/By_Date/" -name "*.png" -delete
     	rm -rf "$USER_HOME/BirdSongs/Extracted/Charts/"*
+
 		HORA_INICIO=$(awk -F'=' '/INICIO_AMANECER/{print $2}' "$CONFIG_HORARIOS" | tr -d ' \r' | tr -d ':')
         HORA_FIN=$(awk -F'=' '/FIN_AMANECER/{print $2}' "$CONFIG_HORARIOS" | tr -d ' \r' | tr -d ':')
         DETECCIONES=$(find "$USER_HOME/BirdSongs/Extracted/By_Date/$(date +%Y-%m-%d)/" -name "*.mp3" 2>/dev/null | grep -oP "birdnet-\K[0-9]{2}:[0-9]{2}" | awk -F: -v ini="$HORA_INICIO" -v fin="$HORA_FIN" '{t=$1*100+$2; if(t>=ini && t<=fin) print}' | wc -l)
+
         python3 "$BASE_PATH/python/log_sistema.py" SIN_CONEXION amanecer $DETECCIONES
+
         bash "$BASE_PATH/scripts/auto_sync_horarios.sh"
+
         HORA_WAKE=$(awk -F'=' '/INICIO_ATARDECER/{print $2}' "$CONFIG_HORARIOS" | tr -d '\r')
         python3 "$BASE_PATH/python/set_wake_pijuice.py" $HORA_WAKE
+        python3 "$BASE_PATH/python/log_sistema.py" MSG "Próxima ventana: atardecer a las $HORA_WAKE"
+        python3 "$BASE_PATH/python/log_sistema.py" MSG "Alarma programada para $HORA_WAKE. Apagando."
+
         sudo chown "$REAL_USER:$REAL_USER" "$USER_HOME/.config/rclone/rclone.conf"
+
         python3 -c "
 import sys
 sys.path.append('/home/lsd/BirdNET-Pi/PiJuice/Software/Source')
@@ -54,7 +63,6 @@ pj.power.SetPowerOff(30)
 	python3 "$BASE_PATH/python/sync_pijuice_rtc.py"
 
 	find "$USER_HOME/BirdSongs/Extracted/By_Date/" -name "*.png" -delete
-
 	rm -rf "$USER_HOME/BirdSongs/Extracted/Charts/"*
 
 	rclone copy "$USER_HOME/BirdSongs/Extracted/By_Date/" "gdrive:$DRIVE_PATH/BirdNET_Detecciones" --include "*.mp3"
@@ -68,7 +76,6 @@ pj.power.SetPowerOff(30)
 	bash "$BASE_PATH/scripts/auto_sync_horarios.sh"
 
 	rclone copy "gdrive:$DRIVE_PATH/config_horarios.txt" "$BASE_PATH/config/"
-
 	rclone copy "$BASE_PATH/log_sistema.txt" "gdrive:$DRIVE_PATH/"
 
 	sudo nmcli radio wifi off
@@ -77,9 +84,13 @@ pj.power.SetPowerOff(30)
 	#sudo sh -c 'echo 0 > /sys/class/rtc/rtc0/wakealarm'
 	#sudo sh -c "echo $(date -u  +%s -d "today $HORA_WAKE") > /sys/class/rtc/rtc0/wakealarm"
 	#sudo halt
+
 	sudo chown "$REAL_USER:$REAL_USER" "$USER_HOME/.config/rclone/rclone.conf"
 
 	python3 "$BASE_PATH/python/set_wake_pijuice.py" $HORA_WAKE
+	python3 "$BASE_PATH/python/log_sistema.py" MSG "Próxima ventana: atardecer a las $HORA_WAKE"
+	python3 "$BASE_PATH/python/log_sistema.py" MSG "Alarma programada para $HORA_WAKE. Apagando."
+
 	python3 -c "
 import sys
 sys.path.append('/home/lsd/BirdNET-Pi/PiJuice/Software/Source')
